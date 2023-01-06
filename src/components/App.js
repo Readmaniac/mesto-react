c44a4b689c558873c46aba9204fa504b49a7ccb4
Stanislav Simakov <stanislav.sima@yandex.ru>
2023-01-06 23:30:12 +0300

    working on context

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -7,6 +7,7 @@ import PopupProfile from './PopupProfile';
 import PopupCreateCard from './PopupCreateCard';
 import PopupDeleteCard from './PopupDeleteCard';
 import PopupImage from './PopupImage';
+import CurrentUserContext from '../contexts/CurrentUserContext';
 
 
 function App() {
@@ -15,6 +16,17 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false)
   const [selectedCard, setSelectedCard] = useState({})
+  const [currentUser, setCurrentUser] = useState()
+
+  useEffect(() => {
+    function handleCurrentUser(){
+      api.getUsersInfo()
+        .then((userData) => {
+          setCurrentUser(userData)
+        })
+    }
+  }, [])
+  
 
   function handleEditAvatarClick(){
     setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen)
@@ -41,6 +53,7 @@ function App() {
 
   return (
     <div className="page">
+      <CurrentUserContext valure={currentUser}>
       <Header />
       <Main 
         onEditProfile={handleEditProfileClick} 
@@ -48,6 +61,7 @@ function App() {
         onEditAvatar={handleEditAvatarClick}
         onCardClick={handleCardClick}
       />
+      </CurrentUserContext>
       <Footer />
       <PopupAvatar isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}/>
       <PopupProfile isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}/>
---
+++ src/contexts/CurrentUserContext.js
@@ -0,0 +1,9 @@
+import avatar from '../images/Avatar.svg'
+
+export const user = {
+  avatar: avatar,
+  name: 'Жак-Ив Кусто',
+  about: 'Исследователь океана'
+}; 
+
+export const UserContext = React.createContext();
